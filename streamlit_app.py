abadb399506e483d577dff4addb0b35672bc2a21
Muhammad Wafa <muhammadwafa105@gmail.com>
2024-01-01 12:09:03 +0700

    feat: copy resul to clipboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 from miriko import Miriko
+import pyperclip
 
 #
 # Initialize
@@ -15,11 +16,14 @@ if "openai_model" not in st.session_state:
 # setup miriko
 miriko = Miriko(st.session_state["openai_model"], client)
 
-    
+# display title    
 st.title("Miriko - Prompt Master")
+
+# display initial message
 with st.chat_message("assistant"):
     st.write("Hello, I'm Miriko. I will help you to optimize your prompt.")
-    
+
+# make user can select the model    
 # selected_model = st.selectbox(
 #     "Select a model",
 #     [
@@ -32,26 +36,65 @@ with st.chat_message("assistant"):
 # st.session_state["openai_model"] = selected_model
 # miriko.update_model(st.session_state["openai_model"])
 
-
+# create a form to submit the prompt
 with st.form('prompt_form'):
+    # create a text area for user to enter the prompt
     prompt = st.text_area(
         label = 'What do you need a prompt for?', 
         value = 'I need a prompt for ', 
         placeholder= 'I need a prompt for ...',)
+    
+    # save the submitted state
     submitted = st.form_submit_button('Submit')
     
-    prompt_valid = True if "I need a prompt for" or "I need a prompt to" in prompt else False
-     
+    # if submitted, display the response
     if submitted:
+        # reset the finished state
+        st.session_state["is_finished"] = False
+        
+        # validate the inputted prompt
+        prompt_valid = True if "I need a prompt for" or "I need a prompt to" in prompt else False
+        
+        # if not valid display error
         if not prompt_valid:
             st.error("You need to include \"I need a prompt for\"")
         else:
+            # if valid display submitted message
             st.info('Submitted')
             with st.chat_message("assistant"):
+                # create initial result placeholder
                 message_placeholder = st.empty()
                 full_response = ""
+                
+                # get the response from miriko and display it
                 for response in miriko.chat(prompt):
                     full_response += response.choices[0].delta.content or ""
                     message_placeholder.markdown(full_response + "")
                     message_placeholder.markdown(full_response)
+                message_placeholder.markdown(full_response)
+                
+                # save the response to session state
+                st.session_state["is_finished"] = True
+                st.session_state["full_response"] = full_response
+    else :
+        # if not submitted display the previous response
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            message_placeholder.markdown(st.session_state["full_response"])
+                
+# check is finished state
+is_finished = False if "is_finished" not in st.session_state else st.session_state["is_finished"]
 
+# display the copy button if the response is finished                
+if is_finished:                
+    is_copied = st.button("Copy to clipboard", key="copy")
+    if is_copied:
+        # copy the response to clipboard
+        pyperclip.copy(st.session_state["full_response"])
+        
+        # display success message and update the finished state
+        st.success("Copied to clipboard")
+        st.session_state["is_finished"] = False
+
+st.divider()
+st.write("created by mikirinkode")
