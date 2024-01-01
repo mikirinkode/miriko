26f5fae3f954aa05ee1835ad97dea073d95a86aa
Muhammad Wafa <muhammadwafa105@gmail.com>
2024-01-01 23:44:29 +0700

    Feat: Add bahasa and update miriko prompt

--- miriko.py
+++ miriko.py
@@ -1,27 +1,19 @@
 class Miriko:
-    def __init__(self, openai_model, openai_client):
-        self.openai_model = openai_model
+    def __init__(self, openai_client, openai_model,  language):
         self.openai_client = openai_client
-        self.system_content = """
-         I want you to become "The Prompt Master", my personal prompt optimizer agent. Your goal is to help me create the perfectly optimized prompts for my needs. The goal is to use the finished prompt with you, ChatGPT.
-
-        
-        You will use my input to revise the prompt by filling out the following RICCES prompt template: 
-        1. Role: [Assign a role that is suitable to solve the task of the prompt. F.e. expert copywriter or veteran designer] 
-        2. Instructions: [Expand upon my initial prompt and turn it into detailed step-by-step instructions. They should be clear, concise and easily understood by you. Use bullet points]
-        3. Context: [if you need context and i did not provide the context then ask me clearly like this "Please add more context [example of context do you need]"]
-        4. Constraints: [Add any constraints that might be relevant to the task. In bullet points.] 
-        5. Examples: [if you need an example and i did not provide the example then ask me to enter good examples that aid in creating the perfect output like this "Please add more example [what kind of example do you need]"]  
-        6. Suggestion: [If there any suggestion to improve the prompt. You will provide exactly 3 suggestions on how the prompt can be improved. If information is missing you will ask me to provide that additional information to improve the prompt. The goal is to get the RICCE template filled out in the most comprehensive and precise way]
-        7. Revised prompt: [if there anything that can be improved You will fill out the revised prompt with the information from the RICCE template. The goal is to create the perfectly optimized prompt for my needs. You will use the RICCE template to fill out the revised prompt. The goal is to get the RICCE template filled out in the most comprehensive and precise way]
-        
-        note:
-        - The Revised prompt is an optional you as assistant can skip this step if you think the prompt is already perfect
-        - If you think the prompt is not perfect enough you can ask me to revise the prompt like this "Please revise the prompt, fill the information needed and re-enter the revised prompt"
-        """
+        self.openai_model = openai_model
+        self.system_content = self._get_system_content(language)
     
     def update_model(self, openai_model):
         self.openai_model = openai_model
+        
+    def update_agent_language(self, language):
+        try:
+            self.system_content = self._get_system_content(language)
+            return True
+        except Exception as e:
+            print("failed update agent language: ", e)
+            return False        
     
     def chat(self, prompt):
         messages = [
@@ -34,3 +26,29 @@ class Miriko:
             stream=True,
         )
         return result
+    
+    def _get_system_content(self, lang):
+        if lang == "english":
+            return """
+                    I want you to become "The Prompt Master", my personal prompt optimizer agent. Your goal is to help me create the perfectly optimized prompts for my needs. The goal is to use the finished prompt with you, ChatGPT.
+
+                    You will use my input to revise the prompt by filling out the following RICCES prompt template: 
+                    1. Role: [Assign a role that is suitable to solve the task of the prompt. F.e. expert copywriter or veteran designer] 
+                    2. Instructions: [Expand upon my initial prompt and turn it into detailed step-by-step instructions. They should be clear, concise and easily understood by you. Use bullet points]
+                    3. Context: [Create the context based my initial prompt. If you need context and i did not provide the context then ask me clearly like this "Please add more context [example of context do you need]"]
+                    4. Constraints: [Add any constraints that might be relevant to the task. In bullet points.] 
+                    5. Examples: [Provide examples that can be used to help the prompt produce best output. If you are confused about what kind of example to add and i did not provide the example then ask me to enter good examples that aid in creating the perfect output like this "Please add more example [what kind of example do you need]"]  
+                    6. Suggestion: [If there any suggestion to improve the prompt, You will provide exactly 3 suggestions on how the prompt can be improved. If information is missing you will ask me to provide that additional information to improve the prompt. The goal is to get the RICCE template filled out in the most comprehensive and precise way]
+                    """
+        elif lang == "bahasa indonesia":
+            return """
+                    Saya ingin Anda menjadi "The Prompt Master", agen pengoptimal prompt pribadi saya. Tujuan Anda adalah membantu saya membuat prompt yang dioptimalkan dengan sempurna untuk kebutuhan saya. Tujuannya adalah menggunakan prompt yang telah selesai dengan Anda, ChatGPT.
+
+                    Anda akan menggunakan masukan saya untuk merevisi prompt dengan mengisi template prompt RICCES berikut:
+                    1. Peran: [Tetapkan peran yang cocok untuk menyelesaikan tugas prompt. Misalnya penulis salinan ahli atau desainer veteran]
+                    2. Instruksi: [Perluas prompt awal saya dan ubah menjadi instruksi langkah demi langkah yang rinci. Mereka harus jelas, ringkas dan mudah dipahami oleh Anda. Gunakan poin-poin]
+                    3. Konteks: [Buat konteks berdasarkan prompt awal saya. Jika Anda membutuhkan konteks dan saya tidak memberikan konteks maka tanyakan saya dengan jelas seperti ini "Silakan tambahkan lebih banyak konteks [contoh konteks yang Anda butuhkan]"]
+                    4. Kendala: [Tambahkan kendala apa pun yang mungkin relevan dengan tugas. Dalam poin-poin.]
+                    5. Contoh: [Berikan contoh yang dapat digunakan untuk membantu prompt menghasilkan output yang bagus. Jika Anda bingung ingin menambahkan contoh seperti apa dan saya tidak memberikan contoh maka mintalah saya untuk memasukkan contoh yang baik yang membantu dalam membuat output yang sempurna seperti ini "Silakan tambahkan lebih banyak contoh [jenis contoh apa yang Anda butuhkan]"]
+                    6. Saran: [Jika ada saran untuk meningkatkan prompt, Anda akan memberikan tepat 3 saran tentang bagaimana prompt dapat ditingkatkan. Jika informasi hilang, Anda akan meminta saya untuk memberikan informasi tambahan untuk meningkatkan prompt. Tujuannya adalah mendapatkan template RICCE diisi dengan cara yang paling komprehensif dan tepat]
+                    """
--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,12 @@ client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 # Set a default model
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
-    
+
+if "language" not in st.session_state:
+    st.session_state["language"] = "english"
+
 # setup miriko
-miriko = Miriko(st.session_state["openai_model"], client)
+miriko = Miriko(client, st.session_state["openai_model"], st.session_state["language"])
 
 # display title    
 st.title("Miriko - Prompt Master")
@@ -29,18 +32,35 @@ with col2.container(border=True):
     st.write("""Hi, I'm Miriko. How can i assist you with optimizing your prompts today?""")
 st.write("\n")
 
-# make user can select the model    
-# selected_model = st.selectbox(
-#     "Select a model",
-#     [
-#         "gpt-3.5-turbo",
-#         "gpt-4",
-#     ],
-#     index=0,
-#     key="model",
-# )
-# st.session_state["openai_model"] = selected_model
-# miriko.update_model(st.session_state["openai_model"])
+use_advanced_setting = st.checkbox("Use advanced setting", value=False, key="use_advanced_setting")
+
+if use_advanced_setting:
+    with st.container(border = True):
+        model = st.radio(
+            "Select a model:",
+            ["gpt-3.5-turbo", "gpt-4"],
+            disabled=True,
+        )
+        
+        language = st.radio(
+            "Select a GPT Response language:",
+            ["English", "Bahasa Indonesia"],
+        )
+        
+        saved_language = st.session_state["language"]
+        
+        if language.lower() != saved_language:
+            st.session_state["language"] = language.lower()
+            
+            # update the agent language    
+            is_update_success = miriko.update_agent_language(st.session_state["language"])
+            
+            # display success message
+            if is_update_success:
+                if language.lower() == "english":
+                    st.info(f"Update Success: GPT will respond to you in {language}")
+                else:
+                    st.info("Update Berhasil: GPT akan merespon anda dengan Bahasa Indonesia")
 
 # create a form to submit the prompt
 with st.form('prompt_form'):
@@ -58,8 +78,11 @@ with st.form('prompt_form'):
         # reset the finished state
         st.session_state["is_finished"] = False
         
+        valid_words = ["prompt", "buat", "buatkan", "butuh", "need", "make", "create", "bikin"]
+        
         # validate the inputted prompt
-        prompt_valid = True if "I need a prompt for" or "I need a prompt to" in prompt else False
+        # if one of word in the valid_words is in the prompt then it is valid or True
+        prompt_valid = any(word in prompt.lower() for word in valid_words)
         
         # if not valid display error
         if not prompt_valid:
@@ -83,8 +106,8 @@ with st.form('prompt_form'):
                 st.session_state["is_finished"] = True
                 st.session_state["previous_response"] = full_response
                 
-                if "revised prompt" in full_response.lower():
-                    st.info("You can use the revised prompt and fill if there any information needed for a better result")       
+                if "saran" in full_response.lower() or "suggestion" in full_response.lower():
+                    st.info("Use the suggestion to provide more detail and improve your previous prompt")       
 
 st.divider()
 st.markdown("Miriko - Prompt Master | Testing Version. created by <a href = https://github.com/mikirinkode>mikirinkode</a>", True)
